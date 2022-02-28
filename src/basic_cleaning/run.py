89c1d4c3030ff36694bb0fd7c680704ddda61aa2
afmansano <afmansano@gmail.com>
2022-02-28 17:37:55 -0300

    Added code to remove invalid coordinates

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     logger.info("Casting last_review datatype to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info('Removing invalid coordinates')
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
     logger.info(f"Artifact saved as {args.output_artifact}")
 
